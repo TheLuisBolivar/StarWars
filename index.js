6e5d4cba86a1c80fc327510949de0d9f784a0ac0
Luis <lebolivarc@gmail.com>
2016-05-09 18:33:27 -0500

    append() en index.js

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // document.getElementByClassName("container");
-$(".container");
+//$(".container");
+
+var machete = window.jQuery;
+machete.get('http://swapi.co/api/planets',function (data) {
+	 //debugger //hacer una pausa en la linea de codigo
+
+	 var planets = data.results;
+
+	 machete(".container").append("<table><tr>");
+
+	 var content = ""
+
+	 for(var x in planets)
+	 	content += "<tr>"+
+	 			       "<td>"+planets[x].name+"</td>"+
+	 			       "<td>"+planets[x].gravity+"</td>"+
+	 			   "</tr>";
+
+	 machete(".container").append("<table class=\"planetas\"><th>Nombre</th><th>Gravedad</th>"+content+"</table>");
+
+	 //planets.results.forEach(callback, function (var){
+	 //	console.log("Info: "+var[0]);
+	 //});
+});
+
